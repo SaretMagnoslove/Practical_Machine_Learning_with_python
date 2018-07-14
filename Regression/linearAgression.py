6d8407fa11924d9f49ce61c05e63f31b8da26998
SaretMagnoslove <magnoslove@gmail.com>
2018-07-14 23:31:43 +0300

    Add Regression Training and Testing

--- Regression/linearAgression.py
+++ Regression/linearAgression.py
@@ -1,6 +1,8 @@
 import pandas as pd
-import quandl
-import math
+import quandl, math
+import numpy as np
+from sklearn import preprocessing, cross_validation, svm
+from sklearn.linear_model import LinearRegression
 
 quandl.ApiConfig.api_key = "YtHWyqZPrpUA26JNZBnV"  # enter your api key
 df = quandl.get('WIKI/GOOGL')  # setting Data Frame
@@ -17,10 +19,42 @@ df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
 # final observation of data
 print(df.head())
 # creating the label: the Adj. Close will be the closing price 10% days into the future
-forecast_col = 'Adj. Close'  # new col to adjust
-df.fillna(value=-99999, inplace=True)  # dealing with NaN
-forecast_out = int(math.ceil(
-    0.01 * len(df)))  # defining # of days to forcast (10%)
-df['label'] = df[forecast_col].shift(-forecast_out)  # the actual label
+
+# new col to adjust
+forecast_col = 'Adj. Close'
+# dealing with NaN
+df.fillna(value=-99999, inplace=True)
+# defining # of days to forcast (10%)
+forecast_out = int(math.ceil(0.01 * len(df)))
+# the actual label
+df['label'] = df[forecast_col].shift(-forecast_out)
+# making sure there are no Na
+df.dropna(inplace=True)
 # print data frame with labels
 print(df.head())
+# defining X as features and y as label
+X = np.array(df.drop(['label'], 1))
+y = np.array(df['label'])
+# scaling X to be between -1 to 1
+preprocessing.scale(X)
+# only X's that has lablels (values for y)
+# X = X[:-forecast_out+1] uneccessery already droped NaNs
+print(len(X), len(y))  # making sure every X has a label
+y = np.array(df['label'])
+# creating train and test sets
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(
+    X, y, test_size=0.2)
+# defining the classifier
+clf = LinearRegression(n_jobs=-1)
+# training the clf.
+clf.fit(X_train, y_train)
+# testing the accuracy
+accuracy = clf.score(X_test, y_test)
+print(accuracy)
+# compared to svm regression
+clf = svm.SVR()
+# training the clf.
+clf.fit(X_train, y_train)
+# testing the accuracy
+accuracy = clf.score(X_test, y_test)
+print(accuracy)
