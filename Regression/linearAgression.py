2b80a8c791297e794cdbb6e7f2aae382215f5472
SaretMagnoslove <magnoslove@gmail.com>
2018-07-15 00:50:20 +0300

    Regression forecasting and predicting

--- Regression/linearAgression.py
+++ Regression/linearAgression.py
@@ -1,8 +1,12 @@
 import pandas as pd
-import quandl, math
+import quandl, math, datetime
 import numpy as np
 from sklearn import preprocessing, cross_validation, svm
 from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt 
+from matplotlib import style
+
+style = 'ggplot'
 
 quandl.ApiConfig.api_key = "YtHWyqZPrpUA26JNZBnV"  # enter your api key
 df = quandl.get('WIKI/GOOGL')  # setting Data Frame
@@ -28,33 +32,37 @@ df.fillna(value=-99999, inplace=True)
 forecast_out = int(math.ceil(0.01 * len(df)))
 # the actual label
 df['label'] = df[forecast_col].shift(-forecast_out)
-# making sure there are no Na
-df.dropna(inplace=True)
-# print data frame with labels
-print(df.head())
-# defining X as features and y as label
+# X for features and y for label
 X = np.array(df.drop(['label'], 1))
+X = preprocessing.scale(X)
+X_lately = X[-forecast_out:]
+X = X[:-forecast_out]
+
+df.dropna(inplace=True)
+
 y = np.array(df['label'])
-# scaling X to be between -1 to 1
-preprocessing.scale(X)
-# only X's that has lablels (values for y)
-# X = X[:-forecast_out+1] uneccessery already droped NaNs
-print(len(X), len(y))  # making sure every X has a label
-y = np.array(df['label'])
-# creating train and test sets
-X_train, X_test, y_train, y_test = cross_validation.train_test_split(
-    X, y, test_size=0.2)
-# defining the classifier
+
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
 clf = LinearRegression(n_jobs=-1)
-# training the clf.
 clf.fit(X_train, y_train)
-# testing the accuracy
-accuracy = clf.score(X_test, y_test)
-print(accuracy)
-# compared to svm regression
-clf = svm.SVR()
-# training the clf.
-clf.fit(X_train, y_train)
-# testing the accuracy
-accuracy = clf.score(X_test, y_test)
-print(accuracy)
+confidence = clf.score(X_test, y_test)
+print(confidence)
+forecast_set = clf.predict(X_lately)
+df['Forecast'] = np.nan
+
+last_date = df.iloc[-1].name
+last_unix = last_date.timestamp()
+one_day = 86400
+next_unix = last_unix + one_day
+
+for i in forecast_set:
+    next_date = datetime.datetime.fromtimestamp(next_unix)
+    next_unix += 86400
+    df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)]+[i]
+
+df['Adj. Close'].plot()
+df['Forecast'].plot()
+plt.legend(loc=4)
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()
