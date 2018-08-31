17d97963ef1e0dcf12f4d71c18e49ed6e4604a02
SaretMagnoslove <magnoslove@gmail.com>
2018-08-31 23:32:30 +0300

    Add How to program the Best Fit Line

--- Regression/manualLinearRegression.py
+++ Regression/manualLinearRegression.py
@@ -1,24 +1,34 @@
 from statistics import mean
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import style
+
+style.use('fivethirtyeight')
 
 # defining the x's and the y's
 xs = np.array([1, 2, 3, 4, 5, 6], dtype=np.float64)
 ys = np.array([5, 4, 6, 5, 6, 7], dtype=np.float64)
-# quick plot of the points of date we defined
-plt.scatter(xs, ys)
-plt.show()
 
 
 # defining a function for finding the best fit slop for y = mx + b
-def best_fit_slop(xs, ys):
-    m = (((mean(xs) * mean(ys)) - mean(xs * ys)) / (np.power(mean(xs), 2) - mean(
-        np.power(xs, 2))))
-    return m
+def best_fit_slop_and_intercept(xs, ys):
+    m = (((mean(xs) * mean(ys)) - mean(xs * ys)) /
+         (np.power(mean(xs), 2) - mean(np.power(xs, 2))))
+    b = mean(ys) - m * mean(xs)
+    return m, b
 
 
 # running the function on the data we have
-m = best_fit_slop(xs, ys)
+m, b = best_fit_slop_and_intercept(xs, ys)
 # printing the best fit\ slope we just found
-print('best fit slop is: {}'.format(m))
+print('best fit slop is: {} and the intercept is {}'.format(m, b))
 
+regression_line = [m * x + b for x in xs]
+
+# predicting y value for a new unseen x
+predict_x = 8
+predict_y = m * predict_x + b
+plt.scatter(xs, ys)
+plt.scatter(predict_x, predict_y, color='g')
+plt.plot(xs, regression_line)
+plt.show()
