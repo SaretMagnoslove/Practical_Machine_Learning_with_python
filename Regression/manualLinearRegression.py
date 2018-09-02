bba0860e2e080d0ee62c6c0c7eac5a6dffc4d56e
SaretMagnoslove <magnoslove@gmail.com>
2018-09-02 23:29:58 +0300

    Add Testing Assumptions

--- Regression/manualLinearRegression.py
+++ Regression/manualLinearRegression.py
@@ -2,12 +2,24 @@ from statistics import mean
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import style
+import random
 
 style.use('fivethirtyeight')
 
 # defining the x's and the y's
-xs = np.array([1, 2, 3, 4, 5, 6], dtype=np.float64)
-ys = np.array([5, 4, 6, 5, 6, 7], dtype=np.float64)
+def create_dataset(hm, variance, step=2, correlation=False):
+    val = 1
+    ys = []
+    for i in range(hm):
+        y = val + random.randrange(-variance, variance)
+        ys.append(y)
+        if correlation and correlation == 'pos':
+            val += step
+        elif correlation and correlation == 'neg':
+            val -= step
+    xs = [i for i in range(len(ys))]
+
+    return np.array(xs, dtype=np.float64), np.array(ys, dtype=np.float64)
 
 
 # defining a function for finding the best fit slop for y = mx + b
@@ -28,6 +40,7 @@ def coefficient_of_determination(y_orig, y_line):
     squared_error_mean = squared_error(y_orig, y_mean_line)
     return 1 - (squared_error_reg / squared_error_mean)
 
+xs, ys = create_dataset(40, 40, 2, correlation='pos')
 
 # running the function on the data we have
 m, b = best_fit_slop_and_intercept(xs, ys)
@@ -40,10 +53,8 @@ regression_line = [m * x + b for x in xs]
 r_squared = coefficient_of_determination(ys, regression_line)
 print('r squared = {}'.format(r_squared))
 
-# predicting y value for a new unseen x
-predict_x = 8
-predict_y = m * predict_x + b
-plt.scatter(xs, ys)
-plt.scatter(predict_x, predict_y, color='g')
-plt.plot(xs, regression_line)
+# plotting the line
+plt.scatter(xs,ys,color='#003F72', label = 'data')
+plt.plot(xs, regression_line, label = 'regression line')
+plt.legend(loc=4)
 plt.show()
