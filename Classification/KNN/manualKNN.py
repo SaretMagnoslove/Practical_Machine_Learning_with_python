8c834cedb34816be6fdd25ace2a63355fdc6d22b
SaretMagnoslove <magnoslove@gmail.com>
2018-09-07 22:30:28 +0300

    Add Writing our own K Nearest Neighbors in Code

--- Classification/KNN/manualKNN.py
+++ Classification/KNN/manualKNN.py
@@ -14,12 +14,24 @@ new_feature = [5, 7]
  for i in dataset]
 plt.scatter(new_feature[0], new_feature[1], s=200, color='g')
 
-plt.show()
+# plt.show()
 
 
 def k_nearest_neighbors(data, predict, k=3):
-    warnings.warn(
-        'value of k is less then number of groups') if len(data) >= k else ('')
+    if len(data) >= k:
+        warnings.warn('value of k is less then number of groups')
 
-    # knnalgos
-    # return vote_result
+    distances = []
+    for group in data:
+        for features in data[group]:
+            euclidean_distance = np.linalg.norm(
+                np.array(features) - np.array(predict))
+            distances.append([euclidean_distance, group])
+
+    votes = [i[1] for i in sorted(distances)[:k]]
+    vote_result = Counter(votes).most_common(1)[0][0]
+    return vote_result
+
+
+result = k_nearest_neighbors(dataset, new_feature, k=3)
+print(result)
