2a886e12c8197c9f831726934e88b18c5c9bb5aa
SaretMagnoslove <magnoslove@gmail.com>
2018-09-07 23:45:45 +0300

    Add Applying our K Nearest Neighbors Algorithm

--- Classification/KNN/manualKNN.py
+++ Classification/KNN/manualKNN.py
@@ -1,20 +1,9 @@
 import numpy as np
 from math import sqrt
-import matplotlib.pyplot as plt
 import warnings
-from matplotlib import style
 from collections import Counter
-
-style.use('fivethirtyeight')
-
-dataset = {'k': [[1, 2], [2, 3], [3, 1]], 'r': [[6, 5], [7, 7], [8, 6]]}
-new_feature = [5, 7]
-
-[[plt.scatter(ii[0], ii[1], s=100, color=i) for ii in dataset[i]]
- for i in dataset]
-plt.scatter(new_feature[0], new_feature[1], s=200, color='g')
-
-# plt.show()
+import pandas as pd
+import random
 
 
 def k_nearest_neighbors(data, predict, k=3):
@@ -33,5 +22,31 @@ def k_nearest_neighbors(data, predict, k=3):
     return vote_result
 
 
-result = k_nearest_neighbors(dataset, new_feature, k=3)
-print(result)
+df = pd.read_csv('breast-cancer-wisconsin.data')
+df.replace('?', -99999, inplace=True)
+df.drop(['id'], 1, inplace=True)
+full_data = df.astype(float).values.tolist()
+
+random.shuffle(full_data)
+test_size = 0.2
+train_set = {2: [], 4: []}
+test_set = {2: [], 4: []}
+train_data = full_data[:-int(test_size * len(full_data))]
+test_data = full_data[-int(test_size * len(full_data)):]
+
+for i in train_data:
+    train_set[i[-1]].append(i[:-1])
+
+for i in test_data:
+    test_set[i[-1]].append(i[:-1])
+
+correct = 0
+total = 0
+
+for group in test_set:
+    for data in test_set[group]:
+        vote = k_nearest_neighbors(train_set, data, k=5)
+        correct = correct + 1 if vote == group else correct
+        total += 1
+
+print("Accuracy: ", correct / total)
