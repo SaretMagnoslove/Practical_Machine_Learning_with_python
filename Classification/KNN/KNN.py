0fc6b396b5ee45f92946abb66ef60a493d215dc0
SaretMagnoslove <magnoslove@gmail.com>
2018-09-08 12:24:36 +0300

    Add Final thoughts on K Nearest Neighbors

--- Classification/KNN/KNN.py
+++ Classification/KNN/KNN.py
@@ -9,17 +9,23 @@ df.drop(['id'], 1, inplace=True)
 X = np.array(df.drop(['class'], 1))
 y = np.array(df['class'])
 
-X_train, X_test, y_train, y_test = cross_validation.train_test_split(
-    X, y, test_size=0.2)
+accuricies = []
+for i in range(25):
+    X_train, X_test, y_train, y_test = cross_validation.train_test_split(
+        X, y, test_size=0.2)
 
-clf = neighbors.KNeighborsClassifier()
-clf.fit(X_train, y_train)
+    clf = neighbors.KNeighborsClassifier(n_jobs=-1)
+    clf.fit(X_train, y_train)
 
-accuracy = clf.score(X_test, y_test)
-print('accuracy = {}'.format(accuracy))
+    accuracy = clf.score(X_test, y_test)
+    print('accuracy = {}'.format(accuracy))
 
-example_measures = np.array([[4, 2, 1, 1, 1, 2, 3, 2, 1]])
-example_measures = example_measures.reshape(len(example_measures), -1)
+    example_measures = np.array([[4, 2, 1, 1, 1, 2, 3, 2, 1]])
+    example_measures = example_measures.reshape(len(example_measures), -1)
 
-prediction = clf.predict(example_measures)
-print('prediction for set: {} is {}'.format(example_measures, prediction))
+    prediction = clf.predict(example_measures)
+    print('prediction for set: {} is {}'.format(example_measures, prediction))
+
+    accuricies.append(accuracy)
+
+print("Avarage Accuracy: ", sum(accuricies) / len(accuricies))
--- Classification/KNN/manualKNN.py
+++ Classification/KNN/manualKNN.py
@@ -19,7 +19,9 @@ def k_nearest_neighbors(data, predict, k=3):
 
     votes = [i[1] for i in sorted(distances)[:k]]
     vote_result = Counter(votes).most_common(1)[0][0]
-    return vote_result
+    confidence = Counter(votes).most_common(1)[0][1] / k
+
+    return vote_result, confidence
 
 
 df = pd.read_csv('breast-cancer-wisconsin.data')
@@ -27,26 +29,33 @@ df.replace('?', -99999, inplace=True)
 df.drop(['id'], 1, inplace=True)
 full_data = df.astype(float).values.tolist()
 
-random.shuffle(full_data)
-test_size = 0.2
-train_set = {2: [], 4: []}
-test_set = {2: [], 4: []}
-train_data = full_data[:-int(test_size * len(full_data))]
-test_data = full_data[-int(test_size * len(full_data)):]
+accuracies = []
 
-for i in train_data:
-    train_set[i[-1]].append(i[:-1])
+for i in range(25):
+    random.shuffle(full_data)
+    test_size = 0.2
+    train_set = {2: [], 4: []}
+    test_set = {2: [], 4: []}
+    train_data = full_data[:-int(test_size * len(full_data))]
+    test_data = full_data[-int(test_size * len(full_data)):]
 
-for i in test_data:
-    test_set[i[-1]].append(i[:-1])
+    for i in train_data:
+        train_set[i[-1]].append(i[:-1])
 
-correct = 0
-total = 0
+    for i in test_data:
+        test_set[i[-1]].append(i[:-1])
 
-for group in test_set:
-    for data in test_set[group]:
-        vote = k_nearest_neighbors(train_set, data, k=5)
-        correct = correct + 1 if vote == group else correct
-        total += 1
+    correct = 0
+    total = 0
 
-print("Accuracy: ", correct / total)
+    for group in test_set:
+        for data in test_set[group]:
+            vote, confidence = k_nearest_neighbors(train_set, data, k=5)
+            correct = correct + 1 if vote == group else correct
+            total += 1
+
+    print("Accuracy: ", correct / total)
+    print("Confidence: ", confidence)
+    accuracies.append(correct / total)
+
+print("Avarage Accuracy: ", sum(accuracies) / len(accuracies))
