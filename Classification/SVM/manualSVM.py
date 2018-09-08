3e935bfac673cadca09a004c6d4648810aa13c70
SaretMagnoslove <magnoslove@gmail.com>
2018-09-08 15:54:37 +0300

    Add SVM Training

--- Classification/SVM/manualSVM.py
+++ Classification/SVM/manualSVM.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from matplotlib import style
 style.use('ggplot')
 
+
 class Support_Vector_Machine:
     def __init__(self, visualization=True):
         self.visualization = visualization
@@ -12,7 +13,41 @@ class Support_Vector_Machine:
             self.ax = self.fig.add_subplot(1, 1, 1)
 
     def fit(self, data):
-        pass
+        self.data = data
+        # { ||w||: [w,b] }
+        opt_dict = {}
+
+        transforms = [[1, 1], [-1, 1], [-1, -1], [1, -1]]
+
+        all_data = []
+        for yi in data:
+            for featureset in data[yi]:
+                for feature in featureset:
+                    all_data.append(feature)
+
+        self.max_feature_value = max(all_data)
+        self.min_feature_value = min(all_data)
+        all_data = None
+
+        step_sizes = [
+            self.max_feature_value * 0.1, self.max_feature_value * 0.01,
+            # point of expanse
+            self.max_feature_value * 0.001
+        ]
+
+        # extremely expansive
+        b_range_multiple = 5
+        # 
+        b_multiple = 5
+
+        latest_optimum = self.max_feature_value * 10
+
+        for step in step_sizes:
+            w = np.array(latest_optimum, latest_optimum)
+            # we can do that because its a convex problem
+            optimized = False
+            while not optimized:
+                pass
 
     def predict(self, features):
         # sign of w.x+b
@@ -24,8 +59,7 @@ class Support_Vector_Machine:
 data_dict = {-1:np.array([[1,7],
                           [2,8],
                           [3,8],]),
-             
+
              1:np.array([[5,1],
                          [6,-1],
                          [7,3],])}
-
