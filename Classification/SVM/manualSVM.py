78802a08a7969136903508ccdecd72d7d504c79a
SaretMagnoslove <magnoslove@gmail.com>
2018-09-14 22:22:02 +0300

    Add SVM Optimization

--- Classification/SVM/manualSVM.py
+++ Classification/SVM/manualSVM.py
@@ -30,14 +30,15 @@ class Support_Vector_Machine:
         all_data = None
 
         step_sizes = [
-            self.max_feature_value * 0.1, self.max_feature_value * 0.01,
+            self.max_feature_value * 0.1,
+            self.max_feature_value * 0.01,
             # point of expanse
             self.max_feature_value * 0.001
         ]
 
         # extremely expansive
         b_range_multiple = 5
-        # 
+        #
         b_multiple = 5
 
         latest_optimum = self.max_feature_value * 10
@@ -47,7 +48,31 @@ class Support_Vector_Machine:
             # we can do that because its a convex problem
             optimized = False
             while not optimized:
-                pass
+                for b in np.arange(
+                        -1 * self.max_feature_value * b_range_multiple,
+                        self.max_feature_value * b_range_multiple,
+                        step * b_multiple):
+                    for tranformation in transforms:
+                        w_t = w * tranformation
+                        found_option = True
+                        for i in self.data:
+                            for xi in self.data[i]:
+                                # under the constraint yi(x1.w+b) > 1
+                                yi = i
+                                if not yi * np.dot(w_t, xi) + b >= 1:
+                                    found_option = False
+                        if found_option:
+                            opt_dict[np.linalg.norm(w_t)] = [w_t, b]
+                if w[0] < 0:
+                    optimized = True
+                    print('optimaized a step')
+                else:
+                    w = w - step
+            norms = sorted([n for n in opt_dict])
+            # ||w|| : [w,b]
+            opt_choice = opt_dict[norms[0]]
+            self.w = opt_choice[0]
+            self.b = opt_choice[1]
 
     def predict(self, features):
         # sign of w.x+b
@@ -56,10 +81,15 @@ class Support_Vector_Machine:
         return classification
 
 
-data_dict = {-1:np.array([[1,7],
-                          [2,8],
-                          [3,8],]),
-
-             1:np.array([[5,1],
-                         [6,-1],
-                         [7,3],])}
+data_dict = {
+    -1: np.array([
+        [1, 7],
+        [2, 8],
+        [3, 8],
+    ]),
+    1: np.array([
+        [5, 1],
+        [6, -1],
+        [7, 3],
+    ])
+}
