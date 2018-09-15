3bfd6aebe9e7599ec32babc786c4125a32cbd456
SaretMagnoslove <magnoslove@gmail.com>
2018-09-15 19:56:41 +0300

    Add Clustering Introduction

--- Clustering/KMeansExample.py
+++ Clustering/KMeansExample.py
@@ -1,8 +1,23 @@
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 from matplotlib import style
 style.use('ggplot')
-import numpy as np 
+import numpy as np
 from sklearn.cluster import KMeans
 
-x = np.random.rand(3,2)*10
-print(x)
+x = np.random.randint(10, size=(60, 2))
+
+# plt.scatter(x[:,0], x[:, 1])
+# plt.show()
+
+clf = KMeans(n_clusters=6)
+clf.fit(x)
+
+centroids = clf.cluster_centers_
+labels = clf.labels_
+
+colors = ['g.', 'r.', 'c.', 'b.', 'k.', 'orange']
+
+for i in range(len(x)):
+    plt.plot(x[i][0], x[i][1], colors[labels[i]], markersize=10)
+plt.scatter(centroids[:, 0], centroids[:, 1], marker='x', s=150, linewidths=5)
+plt.show()
